4868de4064125b3d6c593600500d384532587636
RyanWolfen7 <wolfen7lada@gmail.com>
2023-05-31 10:18:27 -0500

    added to the cloudcannon config

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import tornPaperUp from '../public/effects/tornPaperUp.svg'
 
 const { mainHeader, showTimes, subText } = landingPage
 
-export default function Home() {
+export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className='h-20'>
