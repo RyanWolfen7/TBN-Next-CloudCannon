b56c1829767a094495ce479ffd78f791b7c336a5
RyanWolfen7 <wolfen7lada@gmail.com>
2023-05-31 11:02:55 -0500

    added to the cloudcannon config

--- app/our-guests/[guest]/page.tsx
+++ app/our-guests/[guest]/page.tsx
@@ -2,7 +2,7 @@
 import { useParams } from 'next/navigation';
 
 const GuestPage = ({}) => {
-    const params: Record<string, string | string[]> = useParams();
+    const params: Record<string, string | string[]>  | null= useParams();
     const slug = params?.guest;
     return (
         <h1> Guest {slug} </h1>
