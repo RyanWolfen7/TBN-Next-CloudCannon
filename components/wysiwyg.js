0b62ef24672b99efa07e07eaf9d961d8eeca78f4
RyanWolfen7 <wolfen7lada@gmail.com>
2023-06-01 11:37:20 -0500

    trying visual editor connector

--- components/wysiwyg.js
+++ components/wysiwyg.js
@@ -1,10 +1,15 @@
+import { onCloudCannonChanges, stopCloudCannonChanges } from '@cloudcannon/visual-editor-connector';
+
 const WYSIWYG = ({
     contentHtml
 }) => {
+    onCloudCannonChanges(() => {
+        alert('Works')
+    })
 
     return (
         <div>
-            <div className="post-content"  data-cms-bind="contentHtml" dangerouslySetInnerHTML={{ __html: contentHtml }} />
+            <div className="post-content" data-cms-bind="contentHtml" dangerouslySetInnerHTML={{ __html: contentHtml }} />
         </div>
     )
 }
