7d69faa2a25a6ccdd29cef7692ddf0ba68451cdc
RyanWolfen7 <wolfen7lada@gmail.com>
2023-06-01 11:44:56 -0500

    trying visual editor connector

--- components/wysiwyg.js
+++ components/wysiwyg.js
@@ -1,14 +1,11 @@
-
+'use client'
 const WYSIWYG = ({
     contentHtml
 }) => {
-    if (window.inEditorMode) {
-        alert('Inside CloudCannon!');
-    }
 
     return (
         <div>
-            <div className="post-content" data-cms-bind="contentHtml" dangerouslySetInnerHTML={{ __html: contentHtml }} />
+            <div className="editable post-content" dangerouslySetInnerHTML={{ __html: contentHtml }} />
         </div>
     )
 }
